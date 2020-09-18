207f80ec0d177e3b00a5b06ed9257af05f151677
Anh Minh <47974381+anhminh10a2hoa@users.noreply.github.com>
2020-09-18 21:34:05 +0300

    finished

--- src/store/reducers/alertReducer.ts
+++ src/store/reducers/alertReducer.ts
@@ -8,7 +8,7 @@ export default (state = initialState, action: AlertAction): AlertState => {
   switch (action.type) {
     case SET_ALERT:
       return {
-        message: action.payload,
+        message: 'action.payload',
       };
     default:
       return state;
